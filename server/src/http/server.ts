2b4c449dc7ab4158df5c9e207761847825d4adad
Rogério Viera <rogeriovbh@gmail.com>
2024-10-09 21:43:30 -0300

    create a route for the task creation

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,7 +1,23 @@
 import fastify from "fastify";
+import { createTask } from "../functions/create-task";
+import {z} from "zod"
 
 
 const app = fastify()
+
+app.post('/tasks', async (request) =>{
+  const createTaskSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    //set priority as a literal with 3 possible values, low, medium, hight
+  })
+  const body = createTaskSchema.parse(request.body)
+
+  await createTask({
+    title: body.title,
+    description: body.description,
+  })
+})
 
 app.listen({
   port: 3333,
