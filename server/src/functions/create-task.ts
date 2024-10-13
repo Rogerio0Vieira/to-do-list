ddccc0b2fb57233e1088d9e7993550730c6ea3ae
Rogério Viera <rogeriovbh@gmail.com>
2024-10-13 02:03:06 -0300

    install some functionalityes for fastify and creates a new file for a new function

--- server/src/functions/create-task.ts
+++ server/src/functions/create-task.ts
@@ -3,14 +3,18 @@ import { tasks } from "../db/schema"
 
 interface CreateTaskRequest {
   title: string
-  description: string | null
+  description: string | null,
+  priority: string | null,
+  status: boolean
 }
 
 
-export async function createTask({title, description}: CreateTaskRequest ){
+export async function createTask({title, description, priority, status}: CreateTaskRequest ){
   const result = await db.insert(tasks).values({
     title,
-    description
+    description,
+    priority,
+    status
   }).returning()
 
   const task = result[0]
