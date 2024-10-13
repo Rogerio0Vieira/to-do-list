c53190b291992668cd75a7af0782ecd174090c01
Rogério Viera <rogeriovbh@gmail.com>
2024-10-13 02:01:14 -0300

    modifies type of priority column to receive default values, inserts route of pending tasks

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,21 +1,42 @@
 import fastify from "fastify";
 import { createTask } from "../functions/create-task";
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
 import {z} from "zod"
+import { getWeekPendingTasks } from "../functions/get-week-pending-tasks";
 
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.post('/tasks', async (request) =>{
-  const createTaskSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    //set priority as a literal with 3 possible values, low, medium, hight
-  })
-  const body = createTaskSchema.parse(request.body)
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.get('/pending-tasks', async () =>{
+  const {pendingTasks} = await getWeekPendingTasks()
+  
+  return {pendingTasks}
+})
+
+app.post('/tasks', {
+  schema: {
+    body: z.object({
+      title: z.string(),
+      description: z.string(),
+      status: z.boolean(),
+      priority: z.union([
+        z.literal('low'),
+        z.literal('medium'),
+        z.literal('high'),
+      ]),
+    })
+  }
+} ,  async (request) =>{
+  const {description, title, priority, status} = request.body
 
   await createTask({
-    title: body.title,
-    description: body.description,
+    title,
+    description,
+    priority,
+    status
   })
 })
 
