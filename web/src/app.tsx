782f6e7c5cf28dc193354a0a2e05db1049830dc8
Rogério Viera <rogeriovbh@gmail.com>
2024-10-13 22:42:32 -0300

    import the component to the app file

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,19 +1,17 @@
-import taskIcon from "./assets/tasks-svgrepo-com.svg"
-import { Plus } from "lucide-react"
+import { Dialog } from './components/ui/dialog';
+import { CreateTask } from "./components/create-task";
+import { Summary } from './components/summry';
+// import { EmptyTasks } from './components/empty-tasks';
+
 
 export function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={taskIcon} alt="task-icon" className="size-48" />
-      <p className="text-teal-50 leading-relaxed max-w-80 text-center">
-        You haven't registered any tasks, how about registering one right now?
-      </p>
+    <Dialog >
+      {/* <EmptyTasks/> */}
+      <Summary/>
+      <CreateTask />
+    </Dialog>
 
-      <button type="button" className="px-4 py-2.5 rounded-lg bg-teal-500 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-teal-600" >
-        <Plus className="size-4"/>
-        Register task
-      </button>
-    </div>
   )
 }
 
