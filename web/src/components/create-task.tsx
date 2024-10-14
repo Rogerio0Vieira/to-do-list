e034f24da3b5e4c5bd0d3f1f8096b415690b97f9
Rogério Viera <rogeriovbh@gmail.com>
2024-10-14 02:09:43 -0300

    fix the create task

--- web/src/components/create-task.tsx
+++ web/src/components/create-task.tsx
@@ -1,15 +1,19 @@
-import { X } from "lucide-react";
-import { Button } from "./ui/button";
-import { DialogClose, DialogContent, DialogDescription, DialogTitle } from "./ui/dialog";
-import { Input } from "./ui/input";
-import { Label } from "./ui/label";
-import { RadioGroup, RadioGroupIndicator, RadioGroupItem } from "./ui/radio-goup";
-import { z } from "zod";
-import { Controller, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { createTask } from "../http/create-task";
-import { useQueryClient } from "@tanstack/react-query";
-
+import { X } from 'lucide-react'
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from './ui/dialog'
+import { Label } from './ui/label'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useQueryClient } from '@tanstack/react-query'
+import { createTask } from '../http/create-task'
+import { RadioGroup, RadioGroupIndicator, RadioGroupItem } from './ui/radio-goup'
 
 const createTaskForm = z.object({
   title: z.string().min(1, 'inform task'),
@@ -21,15 +25,17 @@ type CreateTaskForm = z.infer<typeof createTaskForm>
 export function CreateTask() {
   const queryClient = useQueryClient()
 
-  const { register, control, handleSubmit, formState, reset } = useForm({
-    resolver: zodResolver(createTaskForm),
-  })
+  const { register, control, handleSubmit, formState, reset } =
+    useForm<CreateTaskForm>({
+      resolver: zodResolver(createTaskForm),
+    })
 
   async function handleCreateTask(data: CreateTaskForm) {
     await createTask({
       title: data.title,
       desiredWeeklyFrequency: data.desiredWeeklyFrequency,
     })
+
     queryClient.invalidateQueries({ queryKey: ['summary'] })
     queryClient.invalidateQueries({ queryKey: ['pending-tasks'] })
 
@@ -39,96 +45,124 @@ export function CreateTask() {
   return (
     <DialogContent>
       <div className="flex flex-col gap-6 h-full">
-        <div className="flex-col flex-3 gap-3">
-          <div className="flex place-items-center justify-between">
-            <DialogTitle>
-              Register a task
-            </DialogTitle>
-            <DialogClose asChild>
-              <X className="size-5 text-teal-600" />
+        <div className="flex flex-col gap-3">
+          <div className="flex items-center justify-between">
+            <DialogTitle>Cadastrar meta</DialogTitle>
+            <DialogClose>
+              <X className="size-5 text-zinc-600" />
             </DialogClose>
           </div>
+
           <DialogDescription>
-            Add simple or recurring tasks
+            Adicione atividades que te fazem bem e que você quer continuar
+            praticando toda semana.
           </DialogDescription>
         </div>
 
-        <form onSubmit={handleSubmit(handleCreateTask)} className=" flex-1 flex flex-col justify-between">
+        <form
+          onSubmit={handleSubmit(handleCreateTask)}
+          className="flex-1 flex flex-col justify-between"
+        >
           <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-2">
-              <Label htmlFor="title">What is the task?</Label>
-              <Input id="title" autoFocus placeholder="study, do homework, etc..."
+              <Label htmlFor="title">Qual a atividade?</Label>
+              <Input
+                id="title"
+                autoFocus
+                placeholder="study, do homework, etc..."
                 {...register('title')}
               />
-               {formState.errors.title && (
-                <p className="text-teal-400 text-sm">
+
+              {formState.errors.title && (
+                <p className="text-red-400 text-sm">
                   {formState.errors.title.message}
                 </p>
               )}
             </div>
             <div className="flex flex-col gap-2">
-              <Label htmlFor="description">How many times a week ?</Label>
+              <Label htmlFor="title">How many times a week ?</Label>
               <Controller
                 control={control}
                 name="desiredWeeklyFrequency"
-                render={({ field}) => {
+                defaultValue={3}
+                render={({ field }) => {
                   return (
-                    <RadioGroup onValueChange={field.onChange} value={field.value}>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      value={String(field.value)}
+                    >
                       <RadioGroupItem value="1">
                         <RadioGroupIndicator />
-                        <span className="text-teal-300 text-sm font-medium leading-none">1x a weeak</span>
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          1x a weeak
+                        </span>
                         <span className="text-lg leading-none">🥱</span>
                       </RadioGroupItem>
+
                       <RadioGroupItem value="2">
                         <RadioGroupIndicator />
-                        <span className="text-teal-300 text-sm font-medium leading-none">2x a weeak</span>
-                        <span className="text-lg leading-none">👀</span>
-                      </RadioGroupItem>
-                      <RadioGroupItem value="3">
-                        <RadioGroupIndicator />
-                        <span className="text-teal-300 text-sm font-medium leading-none">3x a weeak</span>
-                        <span className="text-lg leading-none">😗</span>
-                      </RadioGroupItem>
-                      <RadioGroupItem value="4">
-                        <RadioGroupIndicator />
-                        <span className="text-teal-300 text-sm font-medium leading-none">4x a weeak</span>
-                        <span className="text-lg leading-none">😮</span>
-                      </RadioGroupItem>
-                      <RadioGroupItem value="5">
-                        <RadioGroupIndicator />
-                        <span className="text-teal-300 text-sm font-medium leading-none">5x a weeak</span>
-                        <span className="text-lg leading-none">😎</span>
-                      </RadioGroupItem>
-                      <RadioGroupItem value="6">
-                        <RadioGroupIndicator />
-                        <span className="text-teal-300 text-sm font-medium leading-none">6x a weeak</span>
-                        <span className="text-lg leading-none">🫡</span>
-                      </RadioGroupItem>
-                      <RadioGroupItem value="7">
-                        <RadioGroupIndicator />
-                        <span className="text-teal-300 text-sm font-medium leading-none">7x a weeak</span>
-                        <span className="text-lg leading-none">🤖</span>
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          2x a weeak
+                        </span>
+                        <span className="text-lg leading-none">🙂</span>
                       </RadioGroupItem>
 
+                      <RadioGroupItem value="3">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          3x a weeak
+                        </span>
+                        <span className="text-lg leading-none">😎</span>
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="4">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          4x a weeak
+                        </span>
+                        <span className="text-lg leading-none">😜</span>
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="5">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          5x a weeak
+                        </span>
+                        <span className="text-lg leading-none">🤨</span>
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="6">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          6x a weeak
+                        </span>
+                        <span className="text-lg leading-none">🤯</span>
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="7">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          Every day
+                        </span>
+                        <span className="text-lg leading-none">🔥</span>
+                      </RadioGroupItem>
                     </RadioGroup>
                   )
                 }}
               />
             </div>
-            <div className="flex flex-col gap-2">
-
-            </div>
-
           </div>
+
           <div className="flex items-center gap-3">
             <DialogClose asChild>
-              <Button type="button" className="flex-1" variant="secondary">Close</Button>
+              <Button type="button" className="flex-1" variant="secondary">
+                Fechar
+              </Button>
             </DialogClose>
-            <Button className="flex-1">Register</Button>
+            <Button className="flex-1">Salvar</Button>
           </div>
         </form>
       </div>
-
     </DialogContent>
   )
 }
