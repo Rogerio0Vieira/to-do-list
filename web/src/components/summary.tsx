ca5f008340143a7c955462c0adce0cf8aa6a3bd1
Rogério Viera <rogeriovbh@gmail.com>
2024-10-14 23:21:08 -0300

    fix

--- web/src/components/summary.tsx
+++ web/src/components/summary.tsx
@@ -1,7 +1,7 @@
 import { CheckCircle2, Plus } from "lucide-react";
 import { Button } from "./ui/button";
 import { DialogTrigger } from "./ui/dialog";
-import  TaskIcon  from "../assets/tasks-svgrepo-com.svg";
+import TaskIcon from "../assets/tasks-svgrepo-com.svg";
 import { Progress, ProgressIndicator } from './ui/progress-bar';
 import { Separator } from "./ui/separator";
 import { useQuery } from "@tanstack/react-query";
@@ -10,7 +10,7 @@ import dayjs from "dayjs";
 import { PendingTasks } from "./pending-tasks";
 
 
-export function Summary(){
+export function Summary() {
   const { data } = useQuery({
     queryKey: ['summary'],
     queryFn: getSummary,
@@ -25,24 +25,24 @@ export function Summary(){
   const lastDayOfWeek = dayjs().endOf('week').format('MMM D')
   const completedPercentage = Math.round(data?.completed * 100 / data?.total)
 
-  return(
+  return (
     <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
       <div className="flex items-center justify-between">
-       <div className="flex items-center gap-3">
+        <div className="flex items-center gap-3">
           <img className="size-7" src={TaskIcon} alt="logo-task" />
           <span className="text-lg font-semibold">{firstDayOfWeek} - {lastDayOfWeek}</span>
-       </div>
+        </div>
         <DialogTrigger asChild>
-            <Button size="sm">
-                <Plus className="size-4"/>
-                Register task
-              </Button>
-          </DialogTrigger>
+          <Button size="sm">
+            <Plus className="size-4" />
+            Register task
+          </Button>
+        </DialogTrigger>
       </div>
 
       <div className="flex flex-col gap-3">
         <Progress value={8} max={15}>
-          <ProgressIndicator style={{width: `${completedPercentage}%`}}/>
+          <ProgressIndicator style={{ width: `${completedPercentage}%` }} />
         </Progress>
 
         <div className="flex items-center justify-between text-xs text-teal-400">
@@ -58,33 +58,33 @@ export function Summary(){
       <div className="flex flex-col gap-6">
         <h2 className="text-x1 font-medium">Your week</h2>
 
-
-        {Object.entries(data.tasksPerDay).map(([date, tasks]) => {
+        {data.tasksPerDay ? (Object?.entries(data?.tasksPerDay).map(([date, tasks]) => {
 
           const weekDay = dayjs(date).format('dddd')
           const formattedDate = dayjs(date).format('MMMM D')
 
-            return(
-              <div key={date} className="flex flex-col gap-4">
-                <h3 className="font-medium">
-                  {weekDay}
-                  <span className="text-teal-400 text-xs">({formattedDate})</span>
-                </h3>
+          return (
+            <div key={date} className="flex flex-col gap-4">
+              <h3 className="font-medium">
+                {weekDay}
+                <span className="text-teal-400 text-xs">({formattedDate})</span>
+              </h3>
 
-                <ul className="flex flex-col gap-3">
-                  {tasks.map(task => {
-                    const time = dayjs(task.completedAt).format('hh:mm A')
-                    return(
-                      <li key={task.id} className="flex items-center gap-2">
-                        <CheckCircle2 className="size-4 text-teal-500" />
-                        <span className="text-sm text-teal-200">You completed "<span className="text-teal-100">{task.title}</span>" at <span className="text-teal-100">{time}</span></span>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
-            )
-          })}
+              <ul className="flex flex-col gap-3">
+                {tasks.map(task => {
+                  const time = dayjs(task.completedAt).format('hh:mm A')
+                  return (
+                    <li key={task.id} className="flex items-center gap-2">
+                      <CheckCircle2 className="size-4 text-teal-500" />
+                      <span className="text-sm text-teal-200">You completed "<span className="text-teal-100">{task.title}</span>" at <span className="text-teal-100">{time}</span></span>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          )
+        })) : null}
+
       </div>
 
     </div>
