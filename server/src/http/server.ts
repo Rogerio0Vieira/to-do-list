384591d524729964b5f43ec81442574b8944bd8e
Rogério Viera <rogeriovbh@gmail.com>
2024-10-14 21:17:12 -0300

    fix the port issue

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -24,7 +24,7 @@ app.register(getWeekSummaryRoute)
 
 
 app.listen({
-  port: 3333,
+  port: process.env.PORT ? Number(process.env.PORT) : 3333,
 }).then(()=>{
   console.log('HTTP sever runing')
 })
